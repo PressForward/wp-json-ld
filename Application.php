32655b7d3b2a18a9bf365d32dba1f1203824ad93
Aram Zucker-Scharff <Aram.Zucker-Scharff@washpost.com>
2018-02-01 18:01:40 -0500

    Setup for takeoff

--- Application.php
+++ Application.php
@@ -9,42 +9,43 @@ $obj = new Loader();
 use Intraxia\Jaxion\Core\Application as JaxionCore;
 
 // use Intraxia\Jaxion\Contract\Core\Application as ApplicationContract;
-class Application extends JaxionCore {
+class Application extends JaxionCore
+{
+    public $ver = JSON_LD_VERSION;
+    /**
+     * ServiceProviders to register with the Application.
+     *
+     * @var string[]
+     */
+    protected $providers = array(
+    'WP_JSON_LD\Core\Providers\Object',
+    );
 
-	public $ver = JSON_LD_VERSION;
-	/**
-	 * ServiceProviders to register with the Application.
-	 *
-	 * @var string[]
-	 */
-	protected $providers = array(
-		'WP_JSON_LD\Core\Providers\Object',
-	);
+    /**
+     * {@inheritdoc}
+     */
+    public function activate()
+    {
+        global $wp_rewrite;
+        define('VERSION', $ver);
+        $current_version = VERSION; // define this constant in the loader file
+        $saved_version = get_option('wp_json_ld_version');
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function activate() {
-		global $wp_rewrite;
-		define( 'VERSION', $ver );
-		$current_version = VERSION; // define this constant in the loader file
-		$saved_version   = get_option( 'pf_version' );
+        // This is a new installation
+        if (!$saved_version) {
+            // Do whatever you need to do during first installation
+            $wp_rewrite->flush_rules(false);
 
-		// This is a new installation
-		if ( ! $saved_version ) {
-			// Do whatever you need to do during first installation
-			$wp_rewrite->flush_rules( false );
+            // This is an upgrade
+        } elseif (version_compare($saved_version, $current_version, '<')) {
+            // Do whatever you need to do on an upgrade
+            $wp_rewrite->flush_rules(false);
+            // Version is up to date - do nothing
+        } else {
+            return;
+        }
 
-			// This is an upgrade
-		} elseif ( version_compare( $saved_version, $current_version, '<' ) ) {
-			// Do whatever you need to do on an upgrade
-			$wp_rewrite->flush_rules( false );
-			// Version is up to date - do nothing
-		} else {
-			return;
-		}
-
-		// Update the version number stored in the db (so this does not run again)
-		update_option( 'pf_version', PF_VERSION );
-	}
+        // Update the version number stored in the db (so this does not run again)
+        update_option('wp_json_ld_version', JSON_LD_VERSION);
+    }
 }
